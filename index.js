2068f50e9cbf6657ac74e6da6a9532ca315baadc
thosd <thos.deaton@gmail.com>
2016-10-20 20:47:40 -0400

    added routing and authorization

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var server = express();
 var port = process.env.PORT || 8080;
+var logger = reuire('./middleware/logger.js');
+var axios = require('axios');
+var cors = require('cors');
+var forecastRouter = require('./routers/forecast.router.js');
+var indexRouter = require('./routers/index.router.js');
 
 server.use(express.static(__dirname + '/public'));
-
-server.get('/', function(request, response){
-  response.sendFile('public/html/index.html', {root: __dirname });
-});
+server.use(logger);
+server.use(cors());
+server.use(indexRouter);
+server.use(forecastRouter);
 
 server.listen(port, function(){
   console.log('Now listening on port...',port);
---
+++ middleware/logger.js
@@ -0,0 +1,9 @@
+function logger(request, response, next){
+  var requestDate = new Date();
+  var requestMethod = request.method;
+  var requestURL = request.url;
+  console.log(requestDate, requestMethod, requestURL);
+  next();
+}
+
+module.exports = logger;
